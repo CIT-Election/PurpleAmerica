2cd1bcd674eba6fef60ff5d39dcfbd5f478dead9
hcps-mosavellb <hcps-mosavellb@180-17mosavellb.hcps.internal>
2016-04-29 15:32:25 -0400

    Attempted to make a program that graphed Alabama in order to see how it worked... didn't work.

--- src/map/PoliticalMap.java
+++ src/map/PoliticalMap.java
@@ -12,13 +12,48 @@
  */
 package map;
 import edu.princeton.cs.introcs.*;
+import java.io.File;
+import java.util.Scanner;
 /**
  *
  * @author 
  */
 public class PoliticalMap {
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception{
         
-        
+     File file = new File("src/map/bama.txt");
+     Scanner scan = new Scanner(file);
+     double latitude; // (φ)
+     double longitude;   // (λ)
+     double mapWidth = 200;
+     double mapHeight = 100;
+     StdDraw.setCanvasSize(200,200);
+     StdDraw.setXscale( 50.9, 51.3);
+     StdDraw.setYscale(32.3, 33);
+     double latRad;
+     double mercN;
+     double[] x = new double[20];
+     double[] y = new double[20];     
+     int i = 0;
+     
+     while(scan.hasNext()){
+         longitude = scan.nextDouble();
+         latitude = scan.nextDouble();
+         scan.nextLine();
+         
+        // get x value
+        x[i] = (longitude+180)*(mapWidth/360);
+
+        // convert from degrees to radians
+        latRad = latitude*(Math.PI)/180;
+
+        // get y value
+        mercN = Math.log(Math.tan(((Math.PI)/4)+(latRad/2)));
+        y[i] = (mapHeight/2)-(mapWidth*mercN/(2*(Math.PI)));
+        i++;
+     }    
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.filledPolygon(x, y);
+
     }
 }
